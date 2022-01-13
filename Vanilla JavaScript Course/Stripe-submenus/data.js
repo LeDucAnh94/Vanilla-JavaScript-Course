e08035ab8415b8b80e3288f899a1d583740e078a
LeDucAnh94 <52671010+LeDucAnh94@users.noreply.github.com>
2022-01-13 15:30:14 +0700

    Add files via upload

---
+++ Vanilla JavaScript Course/Stripe-submenus/data.js
@@ -0,0 +1,28 @@
+const sublinks = [
+    {
+        page:'Products',
+        links: [
+            {label:'payment', icon:'fas fa-credit-card', url: 'index.html'},
+            {label:'terminal', icon:'fas fa-credit-card', url: 'index.html'},
+            {label:'connect', icon:'fas fa-credit-card', url: 'index.html'},
+        ],
+    },
+    {
+        page:'Developers',
+        links: [
+            {label:'plugins', icon:'fas fa-book', url: 'index.html'},
+            {label:'libraries', icon:'fas fa-book', url: 'index.html'},
+            {label:'plugins', icon:'fas fa-book', url: 'index.html'},
+            {label:'billing', icon:'fas fa-book', url: 'index.html'},
+        ],
+    },
+    {
+        page:'Company',
+        links: [
+            {label:'about', icon:'fas fa-briefcase', url: 'index.html'},
+            {label:'customers', icon:'fas fa-briefcase', url: 'index.html'},
+        ],
+    },
+];
+
+export default sublinks;
